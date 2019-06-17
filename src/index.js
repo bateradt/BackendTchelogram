f786c8f1f43daaa77d50ea9867ae6be94a8dc216
bateradt <marcelofscarpim@gmail.com>
2019-06-17 19:54:06 -0300

    Arquivos de configuração do Heroku

--- src/index.js
+++ src/index.js
@@ -18,9 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
-
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
-
 app.use(require('./routes'));
 
 server.listen(process.env.PORT || 3002);
